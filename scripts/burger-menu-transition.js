c953b1c428d4b3c23f093a6c31d588e35f51150b
bitraylee <anshumohanta@hotmail.com>
2021-02-19 01:28:06 +0530

    Added major chuck of the slide out navbar, still needs more tweeks

--- scripts/burger-menu-transition.js
+++ scripts/burger-menu-transition.js
@@ -4,11 +4,15 @@ function changeState(){
    let line2= document.getElementById("line-2");
    let line2Container=document.getElementById("line-2-container");
    let line3= document.getElementById("line-3");
+   let navbarOverlay=document.getElementById("navbar-overlay");
+
    if(currentState=="menu"){
       line1.classList.add("cross-state-1");
       line2Container.classList.add("cross-state-2");
       line2.classList.add("cross-state-2");
       line3.classList.add("cross-state-3");
+      navbarOverlay.style.left="-50px";
+      
 
       currentState="cross"
    }
@@ -17,6 +21,8 @@ function changeState(){
       line2Container.classList.remove("cross-state-2");
       line2.classList.remove("cross-state-2");
       line3.classList.remove("cross-state-3");
+      navbarOverlay.style.left="calc(-100% - 70px)";
+
 
       currentState="menu";
    }
