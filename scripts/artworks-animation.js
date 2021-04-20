96dbffbea3265006621c9a44d9fbb46fe989cf81
bitraylee <anshumohanta@hotmail.com>
2021-04-20 14:23:35 +0530

    Added the final animations to the title-text in artworks section

--- scripts/artworks-animation.js
+++ scripts/artworks-animation.js
@@ -1,5 +1,5 @@
 // gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-let titleArray=["Itachi's last walk", "Woman in the snow", "Lumin\nesence", "Wild Soul", "Ode to winter"];
+let titleArray=["Itachi's last walk", "Woman in the snow", "Luminesence", "Wild Soul", "Ode to winter"];
 let titleTrackNo=0;
 
 const artworksScroller= document.getElementById("scroller");
@@ -8,42 +8,77 @@ let imgContainers=document.querySelectorAll(".index-main-mobile .container .imag
 console.log(imgContainers);
 let title=document.getElementById('title-track');
 
+
+
 imgContainers.forEach((imgContainer, i)=>{
    const current= imgContainer;
    ScrollTrigger.create({
       trigger:container,
       markers:true,
-      start: `${(window.innerHeight * i) - 1} top`,
-      end: `${window.innerHeight*(i+1)} ${window.innerHeight}`,
-      pin:true,
+      start: `${(window.innerHeight * i)} ${window.innerHeight*0.5}`,
+      end: `${window.innerHeight*(i+1)} ${window.innerHeight*0.5}`,
+      // pin:true,
       onEnter: () => {
+         
          titleTrackNo=i+1;
          title.innerHTML=titleArray[titleTrackNo];
          console.log(titleTrackNo);
+
+         gsap.fromTo(title,{
+            opacity:0.2,
+            y:"20px",
+         },{
+            opacity:1,
+            duration:0.5,
+            y:"0px",
+         });
       },
        onLeave: () => {
-         titleTrackNo=i+1;
-
-         title.innerHTML=titleArray[titleTrackNo];
-         console.log(titleTrackNo);
-
-
+         if(i==3){
+            title.innerHTML="";
+            // playExitAnimation();
+         }
+         else{
+            titleTrackNo=i+1;
+            title.innerHTML=titleArray[titleTrackNo];
+            console.log(titleTrackNo);
+         }
+         
+         
       },
-      onEnterBack: () => {             
-         titleTrackNo=i+1;
-
+      onEnterBack: () => { 
+         if(i==1){
+            titleTrackNo=0;
+         }
+         else{
+            titleTrackNo=i+1;
+         }
          title.innerHTML=titleArray[titleTrackNo];
          console.log(titleTrackNo);
-
+         gsap.fromTo(title,{
+            opacity:0.2,
+            y:"20px",
+         },{
+            opacity:1,
+            duration:0.5,
+            y:"0px",
+         });
 
       },
       onLeaveBack: () => {
-         titleTrackNo=i+1;
+         if(i==0){
+            titleTrackNo=0;
+         }
+         else{
+            titleTrackNo=i+1;
+         }
          title.innerHTML=titleArray[titleTrackNo];
          console.log(titleTrackNo);
          
-
+         
       }
    })
-   
 });
+
+
+
